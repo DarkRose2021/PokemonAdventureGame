8b5f5d2a9385f2ef3468bcd54a473318b64b9d60
Katie King <89149647+DarkRose2021@users.noreply.github.com>
2022-08-03 14:13:13 -0600

    made style changes

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const playerName = document.getElementById("PlayerName");
 const submit = document.getElementById("submit");
 var Name;
 
+//starterbtn ids
+const poplio = document.getElementById("poplio");
+const scorbunny = document.getElementById("scorbunny");
+const snivy = document.getElementById("snivy");
+
+const starterInfoDiv = document.getElementById("starterSelection");
+const starterInfo = document.getElementById("starterInfo");
+const btnChoose = document.getElementById("choose");
+
+const h1 = document.getElementById("gameTxtTitle");
+const storyDiv = document.getElementById("storyTxtDiv");
+const story = document.getElementById("storytxt");
+const btnContinue = document.getElementById("Continue");
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     if(playerName.value != ''){
@@ -15,7 +29,53 @@ form.addEventListener("submit", (event) => {
         localStorage.setItem("Name", playerName.value)
         welcome.style.display = "none";
         starters.style.display = "block";
-        //change map to the right map, higher quality img recommended
-        document.body.style.backgroundImage = `url(assets/map.png)`;
     }
+});
+
+poplio.addEventListener("click",() => {
+    starterInfoDiv.style.display = "block";
+    //change
+    starterInfo.innerHTML = "Poplio info text";
+
+    btnChoose.addEventListener("click", () =>{
+        h1.innerHTML = "You chose Poplio";
+        starterInfoDiv.style.display = "none";
+        starters.style.display = "none";
+        storyDiv.style.display = "block";
+        document.body.style.backgroundColor = "#457B9D";
+        document.body.style.backgroundImage = "none";
+        document.body.style.color = "#F1FAEE";
+    });
+});
+
+scorbunny.addEventListener("click", () =>{
+    starterInfoDiv.style.display = "block";
+    //change
+    starterInfo.innerHTML = "Lorem ipsum dolor sit amet. Ut error galisum nam nostrum atque et eius perspiciatis? Id animi tempore qui suscipit voluptates est minus similique ab reiciendis voluptas ut voluptates culpa non repellat sapiente sit autem ratione. Non voluptatibus quod a dolorem modi id maxime quia id veritatis rerum! Est consequatur iste sed quia quod id aspernatur rerum in itaque sequi! A maxime nostrum eum aliquam provident a omnis internos et iure quibusdam quo blanditiis asperiores. Ex voluptas dolores sed neque minus est dolorem placeat rem ullam officiis vel earum autem qui dignissimos quia.Sit repellat adipisci quaerat dolorem quo nihil ratione est omnis modi sit quibusdam eaque eos quia nisi hic perferendis ducimus. Ea sequi facilis qui iure nisi sed error quam.";
+
+    btnChoose.addEventListener("click", () =>{
+        h1.innerHTML = "You chose Scorbunny";
+        starterInfoDiv.style.display = "none";
+        starters.style.display = "none";
+        storyDiv.style.display = "block";
+        document.body.style.backgroundColor = "#C24B57";
+        document.body.style.backgroundImage = "none";
+        document.body.style.color = "#F1FAEE";
+    });
+});
+
+snivy.addEventListener("click", () =>{
+    starterInfoDiv.style.display = "block";
+    //change
+    starterInfo.innerHTML = "snivy info text";
+
+    btnChoose.addEventListener("click", () =>{
+        h1.innerHTML = "You chose Snivy";
+        starterInfoDiv.style.display = "none";
+        starters.style.display = "none";
+        storyDiv.style.display = "block";
+        document.body.style.backgroundColor = "#75C45B";
+        document.body.style.backgroundImage = "none";
+        document.body.style.color = "#F1FAEE";
+    });
 });
